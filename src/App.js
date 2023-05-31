d50ac2e0758b77c256cd807b142c89fdef16c53f
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-05-30 21:23:13 -0400

    created the signup/login compo, front end only

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
-function App() {}
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/Login.css";
+import LogSignIn from "./Components/Signup_login";
+
+function App() {
+  return <LogSignIn />;
+}
 
 export default App;
---
+++ src/Components/Signup_login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function LogSignIn() {
+  return (
+    <header className="login">
+      <div className="loginBtns">
+        <button className="btn btn-black rounded-pill my-button">
+          Sign up
+        </button>
+        <button className="btn btn-white rounded-pill my-button">Log in</button>
+      </div>
+    </header>
+  );
+}
+
+export default LogSignIn;
