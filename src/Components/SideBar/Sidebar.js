b83cf4a91f7239650de2f6f55871a9b640d47eb5
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-05-30 22:55:42 -0400

    working on the sidebar

---
+++ src/Components/SideBar/Sidebar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import spotifyImg from "../../Icons/spotify.png";
+
+function Sidebar() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <img src={spotifyImg} className=""></img>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
