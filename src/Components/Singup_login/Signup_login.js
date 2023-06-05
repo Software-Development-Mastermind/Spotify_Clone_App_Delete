01e0be5ee3e85fb7a7caa4f0af437569bf23728d
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-06-04 20:12:33 -0400

    fixing up the modal card useState logic

--- src/Components/Singup_login/Signup_login.js
+++ src/Components/Singup_login/Signup_login.js
@@ -1,15 +1,64 @@
 import React from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Card from "react-bootstrap/Card";
 
 function LogSignIn() {
+  const [showModal, setShowModal] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleClose = () => setShow(false);
+  const handleShow = (login) => {
+    setIsLogin(login);
+    setShowModal(true);
+  };
+
   return (
-    <header className="login">
-      <div className="loginBtns">
-        <button className="btn btn-black rounded-pill my-button">
-          Sign up
-        </button>
-        <button className="btn btn-white rounded-pill my-button">Log in</button>
-      </div>
-    </header>
+    <>
+      <header className="login">
+        <div className="loginBtns">
+          <button
+            className="btn btn-black rounded-pill my-button"
+            onClick={() => handleShow(true)}
+          >
+            Sign up
+          </button>
+          <button
+            className="btn btn-white rounded-pill my-button"
+            onClick={() => handleShow(false)}
+          >
+            Log in
+          </button>
+        </div>
+      </header>
+
+      {isLogin ? (
+        <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Login</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Here's the login form.</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      ) : (
+        <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Sign Up</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Here's the sign-up form.</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </>
   );
 }
 
