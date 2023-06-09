e2103f1b7b705430decab01e6c362aed9eae8228
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-06-08 20:47:28 -0400

    installed spopify and authlab

--- server/myapp/routes/genre.py
+++ server/myapp/routes/genre.py
@@ -1,34 +1,39 @@
+import spotipy
 from flask import Blueprint, request, jsonify
-import base64
-import requests
-import json
 
 bp = Blueprint('genre', __name__)
 
 CLIENT_ID = "your_spotify_client_id"
 CLIENT_SECRET = "your_spotify_client_secret"
 
-@bp.route('/genre', methods=['POST'])
-def genre():
-    # 1. Obtain a token from Spotify API
-    token_url = "https://accounts.spotify.com/api/token"
-    token_data = {
-        "grant_type": "client_credentials"
-    }
-    client_creds = f"{CLIENT_ID}:{CLIENT_SECRET}"
-    client_creds_b64 = base64.b64encode(client_creds.encode())
-    token_headers = {
-        "Authorization": f"Basic {client_creds_b64.decode()}"
-    }
-    r = requests.post(token_url, data=token_data, headers=token_headers)
-    token_response_data = r.json()
-    access_token = token_response_data['access_token']
+# This could be a dictionary, a database query, etc.
+GENRE_MAP = {
+    1: "hiphop",
+    2: "jazz",
+    3: "metal",
+    4: "pop"
+}
 
-    # 2. Use this token to call the Spotify API
+@bp.route('/genre/<int:id>', methods=['POST'])
+def genre(id):
+    genre = GENRE_MAP.get(id)
+    if not genre:
+        return jsonify({'error': 'Invalid genre ID'}), 400
+
+    # Create a Spotify client object
+    client = spotipy.Spotify()
+
+    # Get the access token
+    token = client.get_access_token(client_id=CLIENT_ID,
+                                 client_secret=CLIENT_SECRET,
+                                 redirect_uri='http://localhost:8080')
+
+    # Make the API request
     headers = {
-        "Authorization": f"Bearer {access_token}"
+        "Authorization": f"Bearer {token}"
     }
-    endpoint = "https://api.spotify.com/v1/browse/new-releases" # change this to the actual Spotify endpoint you want
+    # Use genre in the endpoint, you need to adapt this to match the correct Spotify endpoint
+    endpoint = f"https://api.spotify.com/v1/browse/genres/{genre}" 
     data = requests.get(endpoint, headers=headers).json()
 
     return jsonify(data)
--- server/myapp/routes/register.py
+++ server/myapp/routes/register.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, request, jsonify
+from flask_oauthlib import OAuth2Client
 from werkzeug.security import generate_password_hash
 from users import users  # replace with your actual database module
 
 bp = Blueprint('register', __name__)
+
+oauth = OAuth2Client('google')
 
 @bp.route('/register', methods=['POST'])
 def register():
@@ -13,5 +16,11 @@ def register():
     if username in users:
         return jsonify({'error': 'User already exists'}), 400
 
+    # Create a new user in the database
     users[username] = generate_password_hash(password)
-    return jsonify({'success': 'User created successfully'}), 201
+
+    # Create a new Google OAuth 2 token for the user
+    token = oauth.create_token(username, password)
+
+    # Return the user's token
+    return jsonify({'token': token}), 201
