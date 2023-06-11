92eb0a51343ab84fcbb79cf23105eed2789d0cfc
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-06-11 17:10:43 -0400

    added a link to the randomizerlayout

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import "./Components/SideBar/SideBar.css";
 import "./Components/Layout/Layout.css";
 import "./Components/MusicList/MusicBar.css";
 import "./Components/SearchBar/SearchBar.css";
+import "./Components/Randomizer/RandomizerLayout.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Components/HomePage";
 import SearchBar from "./Components/SearchBar/SearchBar";
+import RandomizerLayout from "./Components/Randomizer/RandomizerLayout";
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/searchbar" element={<SearchBar />} />
+        <Route path="/RandomizerLayout" element={<RandomizerLayout />} />
       </Routes>
     </Router>
   );
---
+++ src/Components/Randomizer/RandomizerLayout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Randomizer from "./Randomizer";
+import Sidebar from "../SideBar/Sidebar";
+import LogSignIn from "../Singup_login/Signup_login";
+
+function RandomizerLayout() {
+  return (
+    <div>
+      <Sidebar />
+      <Randomizer />
+      <LogSignIn />
+    </div>
+  );
+}
+
+export default RandomizerLayout;
