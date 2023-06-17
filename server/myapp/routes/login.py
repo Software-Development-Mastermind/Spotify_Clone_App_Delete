cc8717261ea51710c10222b2923a85fdca9f0f9f
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-06-17 14:45:43 -0400

    trashed the sepreate login page

--- server/myapp/routes/login.py
+++
@@ -1,9 +0,0 @@
-# routes/login.py
-from flask import Blueprint
-
-bp = Blueprint('login', __name__)
-
-@bp.route('/login', methods=['POST'])
-def login():
-    # Implement your login logic here
-    pass
