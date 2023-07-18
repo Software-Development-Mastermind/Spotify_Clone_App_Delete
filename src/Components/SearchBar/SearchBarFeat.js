eafc24834b18f73e38a4bc2cc333e260acd955e0
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-07-17 20:42:30 -0400

    error regarding usestate hook fixed

--- src/Components/SearchBar/SearchBarFeat.js
+++ src/Components/SearchBar/SearchBarFeat.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Card } from "react-bootstrap";
 import SearchBarResults from "./SearchBarResults";
 
-function SearchBarFeat({ setSearchResults, setConfig }) {
+function SearchBarFeat({ setConfig }) {
   const [selectedOption, setSelectedOption] = useState("artist"); // default to 'Artist'
   const [searchQuery, setSearchQuery] = useState(""); // store the search query
   const [startQuery, setStartQuery] = useState("");
