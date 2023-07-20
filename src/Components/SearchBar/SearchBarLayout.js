ea4f435367b7c6a2ed112ac281c65f90d99bed80
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-07-19 22:46:47 -0400

    editing the searchbar layout

--- src/Components/SearchBar/SearchBarLayout.js
+++ src/Components/SearchBar/SearchBarLayout.js
@@ -1,11 +1,18 @@
 import React from "react";
 import LogSignIn from "../Singup_login/Signup_login";
 import Sidebar from "../SideBar/Sidebar";
+import SearchBarFeat from "./SearchBarFeat";
 import SearchBar from "./SearchBar";
 
 function SearchBarLayout() {
+  const [searchResults, setSearchResults] = useState();
+  const [config, setConfig] = useState();
   return (
     <div>
+      <SearchBarFeat
+        setSearchResults={setSearchResults}
+        setConfig={setConfig}
+      />
       <LogSignIn />
       <Sidebar />
       <SearchBar />
