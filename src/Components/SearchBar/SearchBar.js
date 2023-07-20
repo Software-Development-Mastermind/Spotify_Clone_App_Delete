89fb8e82abcf34844f1ce5ec667c0e3b04005c51
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-07-19 22:46:37 -0400

    editing the search bar layout

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,17 +3,13 @@ import Sidebar from "../SideBar/Sidebar";
 import LogSignIn from "../Singup_login/Signup_login";
 import SearchBarFeat from "../SearchBar/SearchBarFeat";
 import SearchBarResults from "./SearchBarResults";
+import SearchBarLayout from "./SearchBarLayout";
 
 function SearchBar() {
-  const [searchResults, setSearchResults] = useState();
-  const [config, setConfig] = useState();
   return (
     <div className="background_color_gradient">
       <LogSignIn />
-      <SearchBarFeat
-        setSearchResults={setSearchResults}
-        setConfig={setConfig}
-      />
+      <SearchBarLayout />
       <SearchBarResults searchResults={searchResults} config={config} />
     </div>
   );
